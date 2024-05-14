291cd454a6aaffce4b14efa0ae15f3df5bb807a2
Andreia <andreiazaharia224@gmail.com>
2024-05-14 16:58:47 +0300

    Project Stage 2

--- proiect_pclp_CATalog.Server/Model/UserModel.cs
+++ proiect_pclp_CATalog.Server/Model/UserModel.cs
@@ -1,10 +1,10 @@
 namespace proiect_pclp_CATalog.Server.Model
-
 {
-    public class UserModelDto
+    public class UserModel
     {
         public int? Id { get; set; }
         public string Login { get; set; }
+        public string? Password { get; set; }
         public string? ShelterName { get; set; }
         public string Role { get; set; }
 
--- proiect_pclp_CATalog.Server/Model/UserModelDto.cs
+++ proiect_pclp_CATalog.Server/Model/UserModelDto.cs
@@ -1,10 +1,10 @@
 namespace proiect_pclp_CATalog.Server.Model
+
 {
-    public class UserModel
+    public class UserModelDto
     {
         public int? Id { get; set; }
         public string Login { get; set; }
-        public string? Password { get; set; }
         public string? ShelterName { get; set; }
         public string Role { get; set; }
 
